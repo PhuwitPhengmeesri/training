f2375550ae8923b946a3a19e819e0e1daf953239
PhuwitPhengmeesri <sb6340248121@lru.ac.th>
2023-03-18 15:56:19 +0700

    1.2

--- project/src/components/HelloWorld.vue
+++ project/src/components/HelloWorld.vue
@@ -1,26 +1,46 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from 'vue';
+import axios from "axios";
+defineProps<{ msg: string }>();
 
-defineProps<{ msg: string }>()
-
-const message = ref('')
-const count = ref(0)
-
+const message = ref('');
+const count = ref(0);
+const list = ref([]);
+const getlist = async function () {
+  const { data } = await axios.get("https://gorest.co.in/public/v2/todos");
+  list.value = data;
+};
 const add = () => {
   count.value++
 }
 </script>
 <template>
-  <h1>{{ msg + ' ' + message}}</h1>
+  <h1 class="text-center" >{{ msg + ' ' + message}}</h1>
+  <div class="card text-center"></div>
+  <div v-for="(item, index) in list" :key="index">
+ 
+    <div class="border-gray-200 bg white px-10 py-10 text-start my-10 rouned-x1">
+    <h3 class="text base font-semibold leading-6 text-gray-900">
+     Title : {{ item["title"] }}
+    </h3>
+    <h3 class="text base font-semibold leading-6 text-gray-900">
+      User : {{ item["sear_id"] }}
+    </h3>
+    <h3 class="text base font-semibold leading-6 text-gray-900">
+     Status : {{ item["status"] }}
+    </h3>
+    </div>
+  </div> 
 
-  <div class="card">
+
     <input type="text" v-model="message">
+    <button type="button" @click="getlist()">GET LIST</button>
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
-  </div>
+ 
 
   <p>
     Check out
@@ -38,6 +58,6 @@ const add = () => {
 
 <style scoped>
 .read-the-docs {
-  color: #888;
+  color: #000000;
 }
 </style>
